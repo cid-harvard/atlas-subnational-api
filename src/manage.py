c41e15628b1f785d12a0fc731d7a1baf81e70956
Mali Akmanalp <mali@akmanalp.com>
2014-12-11 13:29:37 -0500

    Better dummy dataset.

--- src/manage.py
+++ src/manage.py
@@ -2,6 +2,8 @@ from colombia import create_app, models
 import factories
 from flask.ext.script import Manager, Shell
 
+import random
+
 app = create_app()
 manager = Manager(app)
 
@@ -18,15 +20,54 @@ def dummy(n=10):
     if not app.debug:
         raise Exception("Unsafe to generate dummy data while not in DEBUG.")
 
-    ids = []
-    for x in range(0, int(n)):
-        m = factories.HSProduct()
-        ids.append(m.id)
-    models.db.session.commit()
+    # Generate a set of products and departments.
+    departments = []
+    for x in range(0, 10):
+        departments.append(factories.Department())
 
-    print("Inserted these ids:")
-    for x in ids:
-        print(x)
+    products = []
+    for x in range(0, 20):
+        products.append(factories.HSProduct())
+
+    # Generate what products exist in which departments and by how much
+    for d in departments:
+        if random.random() < 0.2:
+            # This place focuses on a few products
+            for x in range(4):
+                factories.DepartmentProductYear(
+                    department=d,
+                    product=random.choice(products),
+                    year=2008
+                )
+        else:
+            # This place is a diverse economy
+            for x in range(20):
+                factories.DepartmentProductYear(
+                    department=d,
+                    product=random.choice(products),
+                    year=2008
+                )
+
+    # Permute data for the following years according to this year.
+    for d in departments:
+        dpys = models.DepartmentProductYear\
+            .query.filter_by(department=d).all()
+        for year in range(2009, 2013):
+            if random.random() < 0.1:
+                delta = random.random() - 0.5
+            else:
+                delta = 5 * (random.random() - 0.5)
+
+            for dpy in dpys:
+                factories.DepartmentProductYear(
+                    department=dpy.department,
+                    product=dpy.product,
+                    year=year,
+                    import_value=dpy.import_value * delta,
+                    export_value=dpy.export_value * delta
+                )
+
+    models.db.session.commit()
 
 
 if __name__ == "__main__":
