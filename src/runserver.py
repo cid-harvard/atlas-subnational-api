3859d9b0b2d14f10364c6ea35c6d8d434c6feb91
Mali Akmanalp <mali@akmanalp.com>
2014-12-19 18:07:52 -0500

    Move runserver stuff in main block

--- src/runserver.py
+++ src/runserver.py
@@ -4,10 +4,11 @@ import sys
 
 app = create_app()
 
-if len(sys.argv) == 2 and sys.argv[1] == "--gevent":
-    from gevent import monkey; monkey.patch_all()
-    from gevent.wsgi import WSGIServer
-    server = WSGIServer(("0",5000), application=app)
-    server.serve_forever()
-else:
-    app.run(port=8080)
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == "--gevent":
+        from gevent import monkey; monkey.patch_all()
+        from gevent.wsgi import WSGIServer
+        server = WSGIServer(("0",5000), application=app)
+        server.serve_forever()
+    else:
+        app.run(port=8080)
