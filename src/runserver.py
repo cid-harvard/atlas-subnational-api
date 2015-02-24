13a050ac02a3885e0382b36aec4d1b9768681396
Mali Akmanalp <mali@akmanalp.com>
2015-02-24 14:37:55 -0500

    Make port configurable.

--- src/runserver.py
+++ src/runserver.py
@@ -6,9 +6,10 @@ app = create_app()
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "--gevent":
+        # TODO: monkey patch should probably happen before everything else
         from gevent import monkey; monkey.patch_all()
         from gevent.wsgi import WSGIServer
         server = WSGIServer(("0",5000), application=app)
         server.serve_forever()
     else:
-        app.run(port=8080)
+        app.run(port=app.config["PORT"])
