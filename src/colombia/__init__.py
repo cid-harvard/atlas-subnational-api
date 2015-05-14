614a1684e2f0af13e0f45c8d529963898e0a866c
Mali Akmanalp <mali@akmanalp.com>
2015-05-14 14:40:13 -0400

    Use sqlalchemy from atlas_core

--- src/colombia/__init__.py
+++ src/colombia/__init__.py
@@ -1,19 +1,13 @@
-from flask import Flask
-
-from flask_debugtoolbar import DebugToolbarExtension
-from werkzeug.contrib.profiler import ProfilerMiddleware
-
 from flask.ext.cache import Cache
 from raven.contrib.flask import Sentry
 from flask.ext import restful
 
-from atlas_core import db as core_db
+import atlas_core
 
 
 class ext(object):
     """Flask extensions."""
 
-    db = core_db
     sentry = Sentry()
     api = restful.Api()
     cache = Cache()
@@ -21,21 +15,18 @@ class ext(object):
     @classmethod
     def reset(cls):
         """To use in unittest teardowns - reset all extensions."""
-        cls.db = core_db
         cls.sentry = Sentry()
         cls.api = restful.Api()
         cls.cache = Cache()
 
 
 def create_app(config={}):
-    app = Flask("colombia")
-    app.config.from_envvar("FLASK_CONFIG")
-    app.config.update(config)
 
-    cache, db, api = ext.cache, ext.db, ext.api
+    # Create base app from atlas_core
+    app = atlas_core.create_app(additional_config=config, name="colombia")
+    cache, api = ext.cache, ext.api
 
     cache.init_app(app)
-    db.init_app(app)
 
     # API Endpoints
     # Background on the URL scheme: https://github.com/cid-harvard/atlas-economic-complexity/issues/125
@@ -78,10 +69,6 @@ def create_app(config={}):
 
     api.init_app(app)
 
-    #from colombia.models import HSProduct
-    #restless_api.init_app(app, flask_sqlalchemy_db=db)
-    #restless_api.create_api(HSProduct, ['GET'], exclude_columns=["en", "es"],
-    #                        results_per_page=-1)
 
     # CORS hook for debug reasons.
     @app.after_request
@@ -91,14 +78,12 @@ def create_app(config={}):
         return response
 
     with app.app_context():
-        db.create_all()
+        # Register sqlalchemy model base so that models in this project that
+        # use it are also registered
+        atlas_core.db.register_base(atlas_core.sqlalchemy.BaseModel)
 
-    # Debug tools
-    if app.debug:
-        DebugToolbarExtension(app)
-        if app.config.get("PROFILE", False):
-            app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
-                                              restrictions=[30],
-                                              sort_by=("time", "cumulative"))
+        # Create empty databases if not created
+        if app.debug:
+            atlas_core.create_db(app, atlas_core.db)
 
     return app
