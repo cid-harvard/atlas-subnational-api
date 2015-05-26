537a3c4f83edf6f2d0f22aabf06e7c3321a4f38b
Mali Akmanalp <mali@akmanalp.com>
2015-05-26 12:08:19 -0400

    Fix broken conf path for doc generation

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../'))
 print os.path.abspath('../colombia/')
-os.environ["FLASK_CONFIG"]="../../conf/dev.py"
+os.environ["FLASK_CONFIG"]="../conf/dev.py"
 
 # -- General configuration -----------------------------------------------------
 
