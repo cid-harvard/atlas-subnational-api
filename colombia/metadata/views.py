51264bcc11b87842a2472145ce1ed4dc466f4532
Mali Akmanalp <mali@akmanalp.com>
2015-05-26 11:55:57 -0400

    Metadata API endpoint generator

--- colombia/metadata/views.py
+++ colombia/metadata/views.py
@@ -7,43 +7,66 @@ from .. import api_schemas as schemas
 metadata_app = Blueprint("metadata", __name__)
 
 
-@metadata_app.route("/products/<int:product_id>")
-@metadata_app.route("/products/", defaults={"product_id": None})
-def product(product_id):
-    """Get all :py:class:`~colombia.models.HSProduct` s or a single one with the
-    given id.
+def make_metadata_api(metadata_class):
+    """Since all metadata APIs look very similar, this function just generates
+    the function that'll handle the API endpoint for an entity. It generates a
+    function that handles both /metadata/entity/ and /metadata/entity/<id>."""
 
-    :param id: Entity id, see :py:class:`colombia.models.Metadata.id`
-    :type id: int
-    :code 404: product doesn't exist
-    """
-    q = HSProduct.query
-    if product_id:
-        q = q.first_or_abort(product_id)
-        return marshal(schemas.metadata, q, many=False)
-    else:
-        level = request.args.get("level", None)
-        q = q.filter_by_enum(HSProduct.level, level)
-        return marshal(schemas.metadata, q)
+    def metadata_api(entity_id):
+        """Get all :py:class:`~colombia.models.Metadata` s or a single one with the
+        given id.
 
+        :param id: Entity id, see :py:class:`colombia.models.Metadata.id`
+        :type id: int
+        :code 404: Entity doesn't exist
+        """
+        q = metadata_class.query
 
-
-@metadata_app.route("/departments/<int:department_id>")
-def department(department_id):
-    """Get a :py:class:`~colombia.models.Department` with the given code.
-
-    :param code: See :py:class:`colombia.models.Department.code`
-    :type code: int
-    :code 404: department doesn't exist
-    """
-    q = Department.query.filter_by(id=department_id)\
-        .first_or_abort(department_id)
-    return marshal(schemas.department, q, many=False)
+        if entity_id:
+            q = q.first_or_abort(entity_id)
+            return marshal(schemas.metadata, q, many=False)
+        else:
+            level = request.args.get("level", None)
+            q = q.filter_by_enum(metadata_class.level, level)
+            return marshal(schemas.metadata, q)
+    return metadata_api
 
 
-@metadata_app.route("/departments/")
-def departments():
-    """Get all the :py:class:`~colombia.models.Department` s."""
-    q = Department.query.all()
-    return marshal(schemas.department, q)
+def register_metadata_apis():
+    """Given an entity class, generate an API handler and register URL routes
+    with flask. """
 
+    for entity_name, settings in metadata_apis.items():
+
+        # Generate handler function for entity
+        api_func = make_metadata_api(settings["entity_class"])
+
+        # Singular endpoint e.g. /entity/7
+        metadata_app.add_url_rule(
+            "/{entity_name}/<int:entity_id>".format(
+                entity_name=settings["plural"]),
+            endpoint=entity_name,
+            view_func=api_func)
+
+        # List endpoint e.g. /entity/
+        metadata_app.add_url_rule(
+            "/{entity_name}/".format(entity_name=settings["plural"]),
+            endpoint=entity_name,
+            view_func=api_func,
+            defaults={"entity_id": None})
+
+        settings["api"] = api_func
+
+
+metadata_apis = {
+    "product": {
+        "entity_class": HSProduct,
+        "plural": "products",
+    },
+    "department": {
+        "entity_class": Department,
+        "plural": "departments",
+    },
+}
+
+register_metadata_apis()
