447073d33db284e6be136b988db56c8b375e31f1
Mali Akmanalp <mali@akmanalp.com>
2015-06-30 15:59:09 -0400

    Fill in / cut / translate all datasets in a common way

--- colombia/import.py
+++ colombia/import.py
@@ -18,6 +18,9 @@ def fillin(df, entities):
         pd.MultiIndex.from_product(df.index.levels, names=df.index.names))
 
 
+def cut_columns(df, columns):
+    return df[list(columns)]
+
 # Classification.merge_to_table
 # Classification.merge_index
 def merge_to_table(classification, classification_name, df, merge_on):
@@ -53,6 +56,36 @@ aduanas_to_atlas = {
     "cog_natl": "cog",
     "RCA_natl": "export_rca"
 }
+
+gdp_to_atlas = {
+    "depcode": "department",
+    "depgdpn": "gdp_nominal",
+    "gdpkmultipliedbydeflator": "gdp_real",
+    "year": "year"
+}
+
+
+pop_to_atlas = {
+    "year": "year",
+    "dp": "department",
+    "popdept": "population"
+}
+
+pila_to_atlas = {
+    "r": "department",
+    "i": "industry",
+    "year": "year",
+    "E_yir": "employment",
+    "W_yir": "wages",
+    "rca": "rca",
+    "density": "density",
+    "cog": "cog",
+    "coi": "coi",
+    "pci": "complexity"
+}
+
+pila_to_atlas_muni = dict(pila_to_atlas.items())
+pila_to_atlas_muni["r"] = "municipality"
 
 
 class ImporterTestCase(BaseTestCase):
@@ -210,23 +243,33 @@ if __name__ == "__main__":
 
             # Department product year
             df = pd.read_stata("/Users/makmana/ciddata/Aduanas/exp_ecomplexity_dpto_oldstata.dta")
-            df.p = df.p.astype(int).astype(str).str.zfill(4)
             df = translate_columns(df, aduanas_to_atlas)
+            df = cut_columns(df, aduanas_to_atlas.values())
+            df["product"] = df["product"].astype(int).astype(str).str.zfill(4)
 
             # Cleaning notes
             # ==============
             # Fix column names
-            # Rectangularize by entity fields
+            # Cut columns
             # Fix types
+
+            # Prefiltering
+
+            # Rectangularize by facet fields? If this comes from classification, do this later
             # Fill digit numbers on classification fields if necessary
             # Merge classification fields, convert from code to ID
 
-            # Group by entities to get subdata
-            # Aggregate each subdata
+            # Group by entities to get facets
+            # Aggregate each facets
             # - eci / pci first()
             # - generate rank fields rank(method='dense')
             # - export_value sum()
-            # Load subdata to given model
+            # Filterations on facets
+
+            # Merge similar facet data (DY datasets together, etc)
+            # Load merged facet to given model
+
+            # Generate other cross-dataset columns: gdp per capita
 
 
             # Dataset:
@@ -238,7 +281,6 @@ if __name__ == "__main__":
 
 
             df = fillin(df, ["department", "product", "year"]).reset_index()
-            from IPython import embed; embed()
 
             df = merge_to_table(product_classification.level("4digit"),
                                 "product_id", df, "product")
@@ -250,25 +292,26 @@ if __name__ == "__main__":
             py.to_sql("product_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
 
-
             # GDP data
             gdp_df = pd.read_stata("/Users/makmana/ciddata/metadata/Annual GDP (nominal)/COL_nomrealgdp_dept_annual1990-2012.dta")
-            gdp_df = gdp_df[["depcode", "year", "depgdpn", "gdpkmultipliedbydeflator"]]
-            gdp_df.columns = ["department", "year", "gdp_real", "gdp_nominal"]
+            gdp_df = translate_columns(gdp_df, gdp_to_atlas)
+            gdp_df = cut_columns(gdp_df, gdp_to_atlas.values())
+
             gdp_df.gdp_real = gdp_df.gdp_real * (10 ** 6)
             gdp_df.gdp_nominal = gdp_df.gdp_nominal * (10 ** 6)
             gdp_df.department = gdp_df.department.astype(str).str.zfill(2)
 
             # Pop data
             pop_df = pd.read_stata("/Users/makmana/ciddata/metadata/Population/COL_pop_deptmunicip_1985-2012.dta")
-            pop_df = pop_df[["year", "dp", "popdept"]]
-            pop_df.columns = ["year", "department", "population"]
+            pop_df = translate_columns(pop_df, pop_to_atlas)
+            pop_df = cut_columns(pop_df, pop_to_atlas.values())
+
+            pop_df = pop_df.groupby(["department", "year"])[["population"]].first().reset_index()
+
             pop_df.department = pop_df.department.astype(str).str.zfill(2)
             pop_df = pop_df[(2007 <= pop_df.year) & (pop_df.year <= 2013)]
-            pop_df = pop_df[~pop_df.duplicated()]
 
             cy = df.groupby(["department", "year"])[["eci", "department_id"]].first().reset_index()
-            cy = fillin(cy, ["department", "year"]).reset_index()
             cy = cy.merge(gdp_df,
                           on=["department", "year"],
                           how="left")
@@ -278,53 +321,57 @@ if __name__ == "__main__":
             cy["gdp_pc_real"] = cy.gdp_real / cy.population
             cy["gdp_pc_nominal"] = cy.gdp_nominal / cy.population
 
+            # TODO: In the real thing, we won't need this "department" column
+            # here because we won't need to merge based on code because
+            # everything will have been converted to IDs already.
             cy = cy.drop("department", axis=1)
             cy.to_sql("department_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
 
-
-            df = df[["department_id", "product_id", "year", "export_value",
-                     "export_rca", "density", "cog", "coi"]]
+            df = df.groupby(["department_id", "product_id", "year"])[["export_value", "export_rca", "density", "cog", "coi"]].first().reset_index()
             df.to_sql("department_product_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
 
 
             # Department - industry - year
             df = pd.read_stata("/Users/makmana/ciddata/PILA_andres/COL_PILA_ecomp-E_yir_2008-2012_rev3_dpto.dta")
-            df = df[["year", "r", "i", "E_yir", "W_yir", "rca", "density", "cog", "coi", "pci"]]
-            df = df[df.i != "."]
+            df = translate_columns(df, pila_to_atlas)
+            df = cut_columns(df, pila_to_atlas.values())
+
+            df = df[df.industry != "."]
+
+            df = fillin(df, ["department", "industry", "year"]).reset_index()
 
             df = merge_to_table(industry_classification.level("class"),
-                                "industry_id", df, "i")
+                                "industry_id", df, "industry")
             df = merge_to_table(location_classification.level("department"),
-                                "department_id", df, "r")
+                                "department_id", df, "department")
 
             # Industry - Year
-            iy = df.groupby(["industry_id", "year"])[["pci"]].first().reset_index()
-            iy = iy.rename(columns={"pci": "complexity"})
+            iy = df.groupby(["industry_id", "year"])[["complexity"]].first().reset_index()
             iy.to_sql("industry_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
 
             # Department - industry - year
-            df = df.rename(columns={"E_yir": "employment", "W_yir": "wages"})
-            df = df[["department_id", "industry_id", "year", "employment",
-                     "wages", "rca", "density", "cog", "coi"]]
+            df = df.groupby(["department_id", "industry_id", "year"])[["employment", "wages", "rca", "density", "cog", "coi"]].first().reset_index()
             df.to_sql("department_industry_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
 
 
             # Municipality - industry - year
             df = pd.read_stata("/Users/makmana/ciddata/PILA_andres/COL_PILA_ecomp-E_yir_2008-2012_rev3_mun.dta")
-            df = df[["year", "r", "i", "E_yir", "W_yir", "rca", "density", "cog", "coi", "pci"]]
-            df = df[df.i != "."]
+            df = translate_columns(df, pila_to_atlas_muni)
+            df = cut_columns(df, pila_to_atlas_muni.values())
+
+            df = df[df.industry != "."]
+
+            df = fillin(df, ["municipality", "industry", "year"]).reset_index()
 
             df = merge_to_table(industry_classification.level("class"),
-                                "industry_id", df, "i")
+                                "industry_id", df, "industry")
             df = merge_to_table(location_classification.level("municipality"),
-                                "municipality_id", df, "r")
+                                "municipality_id", df, "municipality")
 
-            df = df.rename(columns={"E_yir": "employment", "W_yir": "wages"})
-            df = df[["municipality_id", "industry_id", "year", "employment",
-                     "wages", "rca", "density", "cog", "coi"]]
+            df = df.groupby(["municipality_id", "industry_id", "year"])[["employment", "wages", "rca", "density", "cog", "coi"]].first().reset_index()
             df.to_sql("municipality_industry_year", db.engine, index=False,
                       chunksize=10000, if_exists="append")
