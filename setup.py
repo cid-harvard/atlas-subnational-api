f7472954b455f79fbbc864c97cc58ab4b877ebaf
Mali Akmanalp <mali@akmanalp.com>
2015-12-01 13:25:30 -0500

    Revert "Add a minimal setup.py"
    This reverts commit 079863fdeaf693719a105dcf1f2a50f72f192bec.

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name='atlas-subnational-api',
-      url='http://github.com/cid-harvard/atlas-subnational-api/',
-      author='Flying Circus',
-      packages=['colombia'])
