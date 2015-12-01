079863fdeaf693719a105dcf1f2a50f72f192bec
Mali Akmanalp <mali@akmanalp.com>
2015-12-01 12:06:32 -0500

    Add a minimal setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='atlas-subnational-api',
+      url='http://github.com/cid-harvard/atlas-subnational-api/',
+      author='Flying Circus',
+      packages=['colombia'])
