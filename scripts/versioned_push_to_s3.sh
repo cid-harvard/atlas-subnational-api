d0695898362ed96f21af4df9da0e4f3e86bec319
Mali Akmanalp <mali@akmanalp.com>
2016-01-21 15:34:30 -0500

    Delete files in production/ s3 bucket prior to syncing with new folder because otherwise it keeps the metadata of the old files https://github.com/aws/aws-cli/issues/319#issuecomment-25498909 COL-819

--- scripts/versioned_push_to_s3.sh
+++ scripts/versioned_push_to_s3.sh
@@ -24,5 +24,10 @@ aws s3 sync $SOURCE $BUCKETNAME/generated/$FOLDERNAME/ --content-encoding=gzip -
 # Copy in manually uploaded custom files to complete downloads
 aws s3 sync $BUCKETNAME/custom/ $BUCKETNAME/generated/$FOLDERNAME/ --profile $PROFILENAME
 
+# Remove original contents of the production folder because otherwise it keeps
+# the metadata of the original files, which matters for gzip encoded files etc.
+# https://github.com/aws/aws-cli/issues/319#issuecomment-25498909
+aws s3 rm $BUCKETNAME/production/ --recursive --profile $PROFILENAME
+
 # Update the production folder to what we just uploaded and make it public
 aws s3 sync $BUCKETNAME/generated/$FOLDERNAME/ $BUCKETNAME/production/ --acl=public-read --delete --profile $PROFILENAME
