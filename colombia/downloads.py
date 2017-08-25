bd4a1467a5762ea28b54a901a860fd7dbe634582
Mehmet Ali "Mali" Akmanalp <makmanalp@users.noreply.github.com>
2017-08-25 15:08:07 -0400

    Remove unnecessary parentheses.

--- colombia/downloads.py
+++ colombia/downloads.py
@@ -129,7 +129,7 @@ def save_occupations():
     m = ret[('occupation_id', 'industry_id')]
 
     m = merge_classifications(m)
-    m["year"] = current_app.config(["YEAR_MAX_DEMOGRAPHIC"])
+    m["year"] = current_app.config["YEAR_MAX_DEMOGRAPHIC"]
     return m.set_index("year")
 
 
